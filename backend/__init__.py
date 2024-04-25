3a84ff7f39baf1e2f83e89e9dcfcfdafe34d6862
Vinicius Peixoto <nukelet64@gmail.com>
2024-04-25 04:13:04 -0300

    Humongous restructuring of the project layout

---
+++ backend/__init__.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask
+
+from backend import db, api_auth
+
+import secrets
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        DATABASE=os.path.join(app.instance_path, 'tegaki.sqlite')
+    )
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    @app.route("/hello")
+    def hello():
+        return "Hello, World!"
+
+    db.init_app(app)
+    api_auth.init_app(app)
+
+    app.register_blueprint(api_auth.bp)
+
+    return app
---
+++ backend/api_auth.py
@@ -0,0 +1,55 @@
+import functools
+
+from flask import (
+    Blueprint, current_app, flash, g, redirect, render_template,
+    request, session, url_for
+)
+
+from backend.db import get_db
+from werkzeug.exceptions import abort
+
+import click
+import secrets
+
+bp = Blueprint("auth", __name__, url_prefix="/auth")
+
+@bp.route("/", methods=["PUT"])
+def authenticate():
+    user_key = request.form["user_key"]
+    db = get_db()
+
+    if not user_key:
+        abort(403, f"No user_key was provided")
+
+    # verify the provided key
+    result = db.execute(
+        "SELECT * FROM api_auth WHERE user_key = ?",
+        (user_key)
+    ).fetchone()
+
+    if result is None:
+        abort(403, f"Invalid user_key")
+
+    session.clear()
+    session["user_id"] = result["user_id"]
+    return ({"success": True}, 200)
+
+@click.command("generate-api-key")
+def generate_api_key():
+    db = get_db()
+
+    # generate a random 32-bit user id
+    user_id = secrets.randbits(32)
+    # generate a random token with 16 bytes
+    user_key = secrets.token_hex(16)
+
+    db.execute(
+        "INSERT INTO api_auth (user_id, user_key) VALUES (?, ?)",
+        (user_id, user_key)
+    )
+
+    click.echo(f"Your API key: {user_key}")
+    click.echo(f"Your API user ID: {user_id}")
+
+def init_app(app):
+    app.cli.add_command(generate_api_key)
